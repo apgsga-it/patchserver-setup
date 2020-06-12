bbc3fb6624c6960fa1ac4f2398e8ce7c911e2f23
Christoph Henrici <chhenrici@gmail.com>
2020-06-12 22:01:42 +0200

    Ruby Install with Jruby and java gem

--- Puppetfile
+++ Puppetfile
@@ -1,2 +1,3 @@
 mod 'puppetlabs-stdlib', '6.3.0'
 mod 'puppetlabs-java', '6.3.0'
+mod 'jdowning-rbenv', '2.5.0'
---
+++ site-modules/piper/plans/ruby_install.pp
@@ -0,0 +1,15 @@
+plan piper::ruby_install (
+     TargetSpec $targets
+   ) {
+     $targets.apply_prep
+     apply($targets) {
+  	class { 'rbenv':
+  		install_dir => '/opt/rbenv',
+        	latest      => true
+        }
+        rbenv::plugin { [ 'rbenv/rbenv-vars', 'rbenv/ruby-build' ]: }
+        rbenv::build { '2.6.6': global => false }
+	rbenv::build { 'jruby-9.2.11.1': global => true  }
+	rbenv::gem { 'java': ruby_version => 'jruby-9.2.11.1' }
+       }
+}
