1bd07793e52c4b1e79161d60f943fe33091f47a3
Christoph Henrici <chhenrici@gmail.com>
2020-06-15 20:39:15 +0200

    Maven and Gradle Installations

---
+++ site-modules/piper/plans/maven_install.pp
@@ -0,0 +1,48 @@
+plan piper::maven_install (
+     TargetSpec $targets
+   ) {
+     $targets.apply_prep
+     apply($targets) {
+
+	if $facts['osfamily'] != 'windows' {
+ 	  package { 'curl':
+    	    ensure => present,
+  	  }	
+
+          package { 'unzip':
+            ensure => present,
+          }
+
+          Archive {
+            provider => 'curl',
+    	    require  => Package['curl', 'unzip'],
+         } 
+        }
+        
+       	$dirname = 'apache-maven-3.2.2'
+	$filename = "${dirname}-bin.zip"
+	$install_path = "/opt/${dirname}"
+	$profile =  @(EOT) 
+		export MAVEN_HOME=/opt/maven 
+		export PATH=${MAVEN_HOME}/bin:${PATH}
+		|-EOT
+
+
+	archive { $filename:
+ 	 path          => "/tmp/${filename}",
+	 source        => "http://mirror.easyname.ch/apache/maven/binaries/${filename}",
+         extract       => true,
+         extract_path  => '/opt',
+         creates       => $install_path,
+         cleanup       => true
+        } 
+	file { '/opt/maven':
+  		ensure => link,
+  		target => "/opt/${dirname}",
+	}
+	file { '/etc/profile.d/maven.sh':
+	    content => $profile,
+	    mode => '+x'
+	}	
+     }
+} 
