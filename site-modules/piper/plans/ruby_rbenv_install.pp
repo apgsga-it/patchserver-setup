0ee2e608f2d6391cbfa753e18974cd815836145a
Christoph Henrici <chhenrici@gmail.com>
2020-08-25 14:09:07 +0200

    fixes for geminabox

--- site-modules/piper/plans/ruby_rbenv_install.pp
+++ site-modules/piper/plans/ruby_rbenv_install.pp
@@ -3,15 +3,14 @@ plan piper::ruby_rbenv_install(
 ) {
   $targets.apply_prep
   apply($targets) {
-  class { 'rbenv':
+    class { 'rbenv':
       install_dir => '/opt/rbenv',
       latest      => true
-  }
-  rbenv::plugin { [ 'rbenv/rbenv-vars', 'rbenv/ruby-build' ]: }
-  rbenv::build { 'jruby-9.2.13.0': }
-  rbenv::build { '2.6.6': global => true  }
-  rbenv::gem { 'java': ruby_version => 'jruby-9.2.13.0' }
-  rbenv::gem { 'geminabox': ruby_version => '2.6.6', skip_docs => true, timeout => 1200}
-  rbenv::gem { 'thin': ruby_version => '2.6.6', skip_docs => true, timeout => 1200 }
+    }
+    rbenv::plugin { [ 'rbenv/rbenv-vars', 'rbenv/ruby-build' ]: }
+    rbenv::build { '2.6.6': global => true  }
+    rbenv::gem { 'geminabox': ruby_version => '2.6.6', skip_docs => true, timeout => 1200}
+    rbenv::gem { 'thin': ruby_version => '2.6.6', skip_docs => true, timeout => 1200 }
+
   }
 }
