3708799fc6a16a8e604936f27bd713d507165ba3
Julien Helbling <Julien.Helbling@apgsga.ch>
2020-09-29 08:11:22 +0200

    /etc/opt/apg-patch-cli/ops.properties : does not exist anymore

--- site-modules/piper/plans/piper_install.pp
+++ site-modules/piper/plans/piper_install.pp
@@ -4,6 +4,4 @@ plan piper::piper_install (
      $targets.apply_prep
      $yumCmdOptions = "--disablerepo=* --enablerepo=Apg-Piper-Yum*"
      run_command("sudo yum clean all ${yumCmdOptions} && sudo yum -y install ${yumCmdOptions} apg-patch-service-server apg-patch-cli", $targets, '_catch_errors' => true, '_run_as' => 'root')
-  # Quick fix for missing apg user / groups
-    run_command("chmod a+r /etc/opt/apg-patch-cli/ops.properties", $targets, '_catch_errors' => true, '_run_as' => 'root')
 }
