9ca0d226eb863253752e98ecddcd3a37c0e9b54e
jhe <jhe@LAZHG552433282.affichage-p.ch>
2020-11-03 06:10:40 +0100

    correct variable for jenkins cli port

--- site-modules/piper/plans/jenkins_create_jobs.pp
+++ site-modules/piper/plans/jenkins_create_jobs.pp
@@ -12,7 +12,7 @@ plan piper::jenkins_create_jobs (
 				}
      }
      $targetall = get_targets('all')[0]
-     $patch_builder_cmd = "ssh -o \"StrictHostKeyChecking=no\" -l ${targetall.config[ssh][user]}  -p ${targetall.vars[cli_port]} localhost  create-job PatchJobBuilder < <(cat ${patch_builder_template})"
+     $patch_builder_cmd = "ssh -o \"StrictHostKeyChecking=no\" -l ${targetall.config[ssh][user]}  -p ${targetall.vars[jenkins_cli_port]} localhost  create-job PatchJobBuilder < <(cat ${patch_builder_template})"
      run_command("bash -c \'${patch_builder_cmd}\'", $targets, '_catch_errors' => false, '_run_as' => "${targetall.config[ssh][user]}")
 
     $generic_builder_template = "/tmp/genericbuilder-config.xml"
@@ -22,6 +22,6 @@ plan piper::jenkins_create_jobs (
         content => epp('piper/genericbuilder-config.xml.epp', {  })
     }
     }
-    $generic_builder_cmd = "ssh -o \"StrictHostKeyChecking=no\" -l ${targetall.config[ssh][user]}  -p ${targetall.vars[cli_port]} localhost  create-job GenericPipelineJobBuilder < <(cat ${generic_builder_template})"
+    $generic_builder_cmd = "ssh -o \"StrictHostKeyChecking=no\" -l ${targetall.config[ssh][user]}  -p ${targetall.vars[jenkins_cli_port]} localhost  create-job GenericPipelineJobBuilder < <(cat ${generic_builder_template})"
     run_command("bash -c \'${generic_builder_cmd}\'", $targets, '_catch_errors' => false, '_run_as' => "${targetall.config[ssh][user]}")
 }
