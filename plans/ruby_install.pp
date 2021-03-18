0d781492b6a3f6c6583f0fe09cf6ef0d69f067a4
Christoph Henrici <chhenrici@gmail.com>
2021-03-18 16:39:26 +0100

    Ruby versions , enviroment

--- plans/ruby_install.pp
+++ plans/ruby_install.pp
@@ -12,8 +12,8 @@ plan piper::ruby_install(
   rbenv::build { 'jruby-9.2.11.1': global => true  }
   rbenv::gem { 'java': ruby_version => 'jruby-9.2.11.1' }
   rbenv::gem { 'slop': ruby_version => 'jruby-9.2.11.1', version => '>= 4.8.2' }
-  rbenv::gem { 'apscli': ruby_version => 'jruby-9.2.11.1', version => '>= 0.8.2', source => "https://${lookup('gem::user::name')}:${lookup('gem::user::pw')}@${gem_repo}" }
+  rbenv::gem { 'apscli': ruby_version => 'jruby-9.2.11.1', source => "https://${lookup('gem::user::name')}:${lookup('gem::user::pw')}@${gem_repo}" }
   rbenv::gem { 'tty-spinner': ruby_version => 'jruby-9.2.11.1'}
-  rbenv::gem { 'apsmig': ruby_version => 'jruby-9.2.11.1' , version => '>= 0.2.6', source => "https://${lookup('gem::user::name')}:${lookup('gem::user::pw')}@${gem_repo}"}
+  rbenv::gem { 'apsmig': ruby_version => 'jruby-9.2.11.1' , source => "https://${lookup('gem::user::name')}:${lookup('gem::user::pw')}@${gem_repo}"}
   }
 }
